1dc93577e866904596ea59b807c4aeb8e46a5e77
Your Name <you@example.com>
2026-04-21 21:45:16 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const video = document.getElementById("video");
+const result = document.getElementById("result");
+
+// Load models
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("/models")
+
+    
+]).then(startVideo);
+console.log("faceapi:", faceapi);
+
+// Start webcam
+function startVideo() {
+    navigator.mediaDevices.getUserMedia({ video: {} })
+    .then(stream => {
+        video.srcObject = stream;
+    })
+    .catch(err => console.log(err));
+}
+
+let knownDescriptor = null;
+
+// When video starts
+video.addEventListener("play", async () => {
+
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+
+    const displaySize = { width: video.width, height: video.height };
+    faceapi.matchDimensions(canvas, displaySize);
+
+    setInterval(async () => {
+        
+        const detections = await faceapi
+            .detectSingleFace(video, new faceapi.TinyFaceDetectorOptions())
+            .withFaceLandmarks()
+            .withFaceDescriptor();
+
+        if (!detections) {
+            result.innerText = "No face detected";
+            return;
+        }
+
+        // Store reference face
+        if (!knownDescriptor) {
+            knownDescriptor = detections.descriptor;
+            result.innerText = "Reference face stored";
+            return;
+        }
+
+        // Compare
+        const distance = faceapi.euclideanDistance(
+            knownDescriptor,
+            detections.descriptor
+        );
+
+        if (distance < 0.5) {
+            result.innerText = "Match: Same person";
+        } else {
+            result.innerText = "No match";
+        }
+
+    }, 1000);
+});
